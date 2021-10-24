9ec7a743d953b980fdaacd42c308a8faca563b35
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2021-10-24 17:09:00 +0300

    Page-Categories

---
+++ components/PageCategories.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="brands">
+    <div class="global-container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="global-component-header clearfix">
+            <h3 class="global-component-header__title">Kategoriler</h3>
+          </div>
+        </div>
+        <ul class="brand-list home-cat-list clearfix">
+          <li class="brand-list__item">
+            <a href="/cep-telefonu-modelleri/">
+              <span class=""><i class="fas fa-mobile-alt"></i></span>
+              Telefon
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/notebook/">
+              <span class=""><i class="fas fa-laptop"></i></span>
+              Notebook
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/televizyon/">
+              <span class=""><i class="fas fa-tv"></i></span>
+              Televizyon
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/akilli-saatler/">
+              <span class="" style="font-weight: bold"><i class="far fa-clock"></i> </span>
+              Akıllı Saat
+            </a>
+          </li>
+          <li class="brand-list__item hidden-lg hidden-md">
+            <a href="/bilgisayar-bilesenleri/">
+              <span class=""><i class="fas fa-microchip"></i></span>
+              Bilgisayar Parçaları
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/yazici/">
+              <span class=""><i class="fas fa-print"></i></span>
+              Yazıcı
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/oyun-konsollari/">
+              <span class=""> <i class="fas fa-gamepad"></i></span>
+              Oyun Konsolları
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/elektrikli-ev-aletleri/">
+              <span class=""><i class="fas fa-blender"></i> </span>
+              Ev Ürünleri
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/kisisel-bakim-urunleri/">
+              <span class=""><i class="fas fa-shower"></i> </span>
+              Kişisel Bakım
+            </a>
+          </li>
+          <li class="brand-list__item">
+            <a href="/aksesuarlar/">
+              <span class=""><i class="fas fa-headphones"></i></span>
+              Aksesuar
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div> BURASI ANA SAYFADIR</div>
+  <div > 
+    <PageCategories/>
+  </div>
   
   
     
