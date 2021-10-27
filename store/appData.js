54cd0fc832e81469c993b640fec5b07e6843e870
Ysf <91877428+Ysf-Akdnz@users.noreply.github.com>
2021-10-28 00:27:42 +0300

    PageHomeSlider.vue YAPILDI.

---
+++ store/appData.js
@@ -0,0 +1,88 @@
+export default {
+    HomePageSliderSource: [
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/07-2021/gece-note-7-temmuzweb.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/thumb/note-thumb-24-aralik.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/06-2021/gece-tablet-9-haziran-web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/thumb/tablet-kamp-30-mart-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/gece-cep-26-10-web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/gece-cep-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/oppo_ilan_web_revize.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Oppo_tuhnbnail_.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/ILAN_WEB_lenovo_27.10.2021.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Lenovo_Thunbnail.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/INDIRIM-27-10-WEB.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/samsung-tv-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/TCL-tv-27-10-web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/TCL-TV-27-10-THUMB.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Samsung_S20_Fe_Web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Samsung_S20_Fe_Thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/canon-25-10-21-ILAN-WEB.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/canon-21-10-21-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Philips-GC895030-Web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Philips-GC895030-Thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/grunding-supurge-WEB.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/grunding-robot-27-10-THUMB.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/msi_VATAN_web.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/msi_VATAN_thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/b2s_vatan_1336x380.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/canonthmb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/SSD-21-10-21-WEB_-_Kopya.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/SSD-21-10-21-thumb_-_Kopya.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/uni-web-min.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/uni-thumb-min.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/1336x380-Vatan.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/lgoled-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/Neo_Qled_Vatan_1336x380-min.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/tmmm.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/philips-20-10-ILAN_WEB.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/tv-20-10-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/ffd.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/yogatmb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Amd_Ryzen_Notebook-WEB.jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/10-2021/Amd_Ryzen_Notebook-thumb.jpg"
+        },
+        {
+            src: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/08-2021/Vatan_XPS_13_Banner_(1336x380)adchecked_(002).jpg",
+            bulletSrc: "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/thumb/dell-g3-thumb.jpg"
+        },
+    ]
+}
